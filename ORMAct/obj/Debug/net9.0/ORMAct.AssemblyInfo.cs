024254039a38fec41e22ce5ec58d80903f8bba59
mcronERNI <mhike.cron@betterask.erni>
2025-02-07 11:30:10 +0800

    update

--- ORMAct/obj/Debug/net9.0/ORMAct.AssemblyInfo.cs
+++ ORMAct/obj/Debug/net9.0/ORMAct.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORMAct")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2de1d76fcd08c98010a36c5269110ac719580c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d64d73bc460a3269ba7a062256e8ececa87d81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORMAct")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORMAct")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
